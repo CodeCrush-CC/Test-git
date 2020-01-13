16052fd23dd7a70cb4d562e4665c31aee21ae005
Tushar Malhotra <tusharmalhotra494@gmail.com>
2020-01-12 23:03:07 -0800

    here we go again

---
+++ test2.cpp
@@ -0,0 +1,20 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr() ;
+
+	int a, b, c ;
+
+	cout<<"Enter first number:\t" ;
+	cin>> a ;
+
+	cout<<"Enter second number:\t" ;
+	cin  b;
+
+	c = a+ b ;
+
+	cout<<"Sum = "<<c ;
+
+	getch() ;
+}
